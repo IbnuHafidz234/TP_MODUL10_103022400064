3cb3bcec08992fbb89828f90ae9a28df86bf8047
nu <hafidzibnu035@gmail.com>
2026-05-03 09:06:21 +0700

    Menambahkan API Film

---
+++ TP_MODUL10_103022400064/Controllers/FilmController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP_MODUL10_103022400064.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+        // List static untuk menyimpan data film secara temporary
+        private static List<Film> _dataFilm = new List<Film>
+        {
+            new Film("Inception", "Christopher Nolan", "2010", "Sci-Fi", "9.0"),
+            new Film("Interstellar", "Christopher Nolan", "2014", "Sci-Fi", "8.7"),
+            new Film("Parasite", "Bong Joon-ho", "2019", "Thriller", "8.6")
+        };
+
+        // a. GET /api/Film
+        [HttpGet]
+        public IEnumerable<Film> Get()
+        {
+            return _dataFilm;
+        }
+
+        // b. GET /api/Film/{index}
+        [HttpGet("{id}")]
+        public ActionResult<Film> Get(int id)
+        {
+            if (id < 0 || id >= _dataFilm.Count) return NotFound();
+            return _dataFilm[id];
+        }
+
+        // c. POST /api/Film
+        [HttpPost]
+        public void Post([FromBody] Film film)
+        {
+            _dataFilm.Add(film);
+        }
+
+        // d. DELETE /api/Film/{index}
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (id < 0 || id >= _dataFilm.Count) return NotFound();
+            _dataFilm.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
--- TP_MODUL10_103022400064/obj/Debug/net10.0/TP_MODUL10_103022400064.AssemblyInfo.cs
+++ TP_MODUL10_103022400064/obj/Debug/net10.0/TP_MODUL10_103022400064.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL10_103022400064")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a979effa64df473bb854ab2926f9958fb8e19b07")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL10_103022400064")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL10_103022400064")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
